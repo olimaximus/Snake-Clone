054a2375270b71828fd506fe4bec7a7a6a701435
Maxime Le Sage <mlesa084@uottawa.ca>
2025-07-23 17:34:16 -0400

    Add start screen and game over handling

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace Snake_Clone
         private readonly int rows = 15, cols = 15;
         private readonly Image[,] gridImages;
         private GameState gameState;
+        private bool gameRunning;
 
         public MainWindow()
         {
@@ -34,10 +35,29 @@ namespace Snake_Clone
             gameState = new GameState(rows, cols);
         }
 
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        private async Task RunGame()
         {
             Draw();
+            await ShowCountDown();
+            Overlay.Visibility = Visibility.Hidden;
             await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, cols);
+        }
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled = true;
+            }
+
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -113,6 +133,22 @@ namespace Snake_Clone
                     gridImages[r, c].Source = gridValToImage[gridVal];
                 }
             }
+        }
+
+        private async Task ShowCountDown()
+        {
+            for (int i = 3; i > 0; i--)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(500);
+            }
+        }
+
+        private async Task ShowGameOver()
+        {
+            await Task.Delay(1000);
+            Overlay.Visibility = Visibility.Visible;
+            OverlayText.Text = "PRESS ANY KEY TO START";
         }
     }
 }
